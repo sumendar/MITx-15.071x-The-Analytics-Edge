0a9f985d4a10a0249c612c24a80bd2b90919e288
sumendar karupakala <sumendar@gmail.com>
2015-07-27 13:45:02 +0530

    Unit-1--An-Introduction-to-Analytics except Video 6

--- Unit1_IntroductionR.R
+++ Unit1_IntroductionR.R
@@ -2,29 +2,32 @@ setwd("C:/Users/Sri/Documents/GitHub/RWD/MITx 15.071x The Analytics Edge/Unit-1-
 
 # VIDEO 2
 
-# Basic Calculations
-8*16
+########## Basic Calculations########
+8*6
 2^16
 2^
 8*6
 8*10
-
-# Functions
+# 
+######## Functions############
 sqrt(2)
 abs(-65)
 ?sqrt
 
-# Variables
+########## Variables ##########
 SquareRoot2 = sqrt(2)
 SquareRoot2
 HoursYear <- 365*24
 HoursYear
+#### rm(list = ls())  ########### used to delete all objects
 ls()
-
-
-# VIDEO 3 
-
-# Vectors
+######## rm(SquareRoot2) #########  to delete only specific object
+ls()
+# 
+# 
+# # VIDEO 3 
+# 
+##### # Vectors ##########
 c(2,3,5,8,13)
 Country = c("Brazil", "China", "India","Switzerland","USA")
 LifeExpectancy = c(74,76,65,83,79)
@@ -32,10 +35,10 @@ Country
 LifeExpectancy
 Country[1]
 LifeExpectancy[3]
-Sequence = seq(0,100,2)
+Sequence = seq(0,100,2) #another function to create a vector
 Sequence
-
-# Data Frames
+# 
+# ######## Data Frames ########
 CountryData = data.frame(Country, LifeExpectancy)
 CountryData
 CountryData$Population = c(199000,1390000,1240000,7997,318000)
@@ -47,63 +50,61 @@ NewCountryData = data.frame(Country, LifeExpectancy, Population)
 NewCountryData
 AllCountryData = rbind(CountryData, NewCountryData)
 AllCountryData
-
-
-# VIDEO 4
-
-# Loading csv files
-WHO = read.csv("WHO.csv")
+# 
+# 
+# # VIDEO 4
+# 
+# ########### Loading csv files #########
+WHO <- read.csv("WHO.csv")
 str(WHO)
 summary(WHO)
-
-# Subsetting
-WHO_Europe = subset(WHO, Region == "Europe")
+WHO_Europe <- subset(WHO,Region=="Europe") #subset of a dataframe
 str(WHO_Europe)
-
-# Writing csv files
-write.csv(WHO_Europe, "WHO_Europe.csv")
-
-# Removing variables
+write.csv(WHO_Europe,"WHO_Europe.csv") #write any file to the location
+ls()
+# 
+# # Removing variables
 rm(WHO_Europe)
-
-
-# VIDEO 5
-
-# Basic data analysis 
-
-mean(WHO$Under15)
+ls()
+# 
+# 
+# # VIDEO 5
+# 
+# # Basic data analysis 
+# 
+mean(WHO$Under15)  
 sd(WHO$Under15)
 summary(WHO$Under15)
-
 which.min(WHO$Under15)
 WHO$Country[86]
-
 which.max(WHO$Under15)
 WHO$Country[124]
+# 
 
-# Scatterplot
+# 
+
+# 
+
+# 
+# # Subsetting
 plot(WHO$GNI, WHO$FertilityRate)
-
-# Subsetting
-Outliers = subset(WHO, GNI > 10000 & FertilityRate > 2.5) 
+Outliers <-  subset(WHO, GNI > 10000 & FertilityRate>2.5) 
 nrow(Outliers)
 Outliers[c("Country","GNI","FertilityRate")]
+# 
+# 
+# # VIDEO 6
+# 
+# # Histograms
+
+# 
+# # Boxplot
 
 
-# VIDEO 6
+# 
 
-# Histograms
-hist(WHO$CellularSubscribers)
 
-# Boxplot
-boxplot(WHO$LifeExpectancy ~ WHO$Region)
+# 
+# # Summary Tables
 
-boxplot(WHO$LifeExpectancy ~ WHO$Region, xlab = "", ylab = "Life Expectancy", main = "Life Expectancy of Countries by Region")
-
-# Summary Tables
-table(WHO$Region)
-
-tapply(WHO$Over60, WHO$Region, mean)
-tapply(WHO$LiteracyRate, WHO$Region, min)
-tapply(WHO$LiteracyRate, WHO$Region, min, na.rm=TRUE)
- 
+# 
