97e3990f2f727a40513d1c3d3f747e3969a29bb2
sumendar karupakala <sumendar@gmail.com>
2015-08-07 10:44:27 +0530

    up to video 5

--- Unit 2 Linear Regression/The Statistical Sommelier An Introduction to Linear Regression/The Statistical Sommelier An Introduction to Linear Regression.R
+++ Unit 2 Linear Regression/The Statistical Sommelier An Introduction to Linear Regression/The Statistical Sommelier An Introduction to Linear Regression.R
@@ -32,4 +32,23 @@ modelQQ5 <- lm(Price ~ HarvestRain + WinterRain, data = wine)
 summary(modelQQ5)
 ########VIDEO 6: CORRELATION AND MULTICOLLINEARITY############
 # CORRELATION : A measure of the linear relationship between variables
-
+cor(wine$WinterRain,wine$Price) # correlation between winterrain and price
+cor(wine$Age,wine$FrancePop)
+cor(wine) # where we can get all correlation between all two variables by looking cross  check(Row X Column)
+# age and francepop is highly correlated, multicollinearity refers to when two independent var highly correlated
+# but if we have high correlation b/w dependent and independent variable is good thing, bcZ we will predict depedent using independent 
+# you want to remove insignificant var at a time
+model5 <- lm(Price ~ AGST+HarvestRain+WinterRain,data = wine) 
+# we can write as below also for lm function 
+#difference here is in above lm function we mentioned data set name as one argument
+#so no need to specify $ notation, but where as in below function we need to 
+#specify $ notation bcz we are ignoring data set name argument in the function
+model5 <- lm(wine$Price ~ wine$AGST+wine$HarvestRain+wine$WinterRain)
+summary(model5)
+#AGST and HarvestRain is significant, WinterRain is almost significant
+# R squared 0.75
+#######QUICK QUESTION  6##########
+#Using the data set wine.csv, what is the correlation between HarvestRain and WinterRain?
+cor(wine$HarvestRain,wine$WinterRain)
+cor(wine$WinterRain,wine$HarvestRain)
+############VIDEO 7: MAKING PREDICTIONS#################
